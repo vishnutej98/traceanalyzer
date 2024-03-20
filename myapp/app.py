461ba6f7b7c4264516a4f36911d7e3d379c61dd9
Vishnu Tej <68961493+vishnutej98@users.noreply.github.com>
2024-03-20 22:44:16 +0530

    Modified app.py for testing

--- myapp/app.py
+++ myapp/app.py
@@ -1,17 +1,19 @@
 from time import sleep
+import fire
 
 fixed_timer: float = 0.0004
 
-name: str = "Tej"
+# name: str = "Tej"
 
-def mainfunc(name):
+def mainfunc(name='Tej'):
     if name:
         print(f'Hello {name}!')
         sleep(fixed_timer)
         print(f'Have a nice day {name}!')
+        return f"Hello {name}!"
     else:
         ... # Do nothing
 
 
 if __name__ == "__main__":
-    mainfunc(name)
+    fire.Fire(mainfunc)
