6a0ca1678f5cd02e98474158efc4425f4eb51f1c
Vishnu Tej <68961493+vishnutej98@users.noreply.github.com>
2024-03-20 22:40:08 +0530

    Modified jenkinsfile and app.py for testing

--- myapp/app.py
+++ myapp/app.py
@@ -2,6 +2,16 @@ from time import sleep
 
 fixed_timer: float = 0.0004
 
-for _ in range(100):
-    print('Reached the app.py')
-    sleep(fixed_timer)
+name: str = "Tej"
+
+def mainfunc(name):
+    if name:
+        print(f'Hello {name}!')
+        sleep(fixed_timer)
+        print(f'Have a nice day {name}!')
+    else:
+        ... # Do nothing
+
+
+if __name__ == "__main__":
+    mainfunc(name)
