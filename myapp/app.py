d0c4a18bc6f9b8e00276c70d839f1274dec50178
Vishnu Tej <68961493+vishnutej98@users.noreply.github.com>
2024-03-24 12:39:58 +0530

    added the age parameter

--- myapp/app.py
+++ myapp/app.py
@@ -5,12 +5,12 @@ fixed_timer: float = 0.0004
 
 # name: str = "Tej"
 
-def mainfunc(name='Tej'):
+def mainfunc(name='Tej', age=20):
     if name:
-        print(f'Hello {name}!')
+        print(f'Hello {name}! and i believe your are {age} years old.')
         sleep(fixed_timer)
         print(f'Have a nice day {name}!')
-        return f"Hello {name}!"
+        # return f"Hello {name}!"
     else:
         ... # Do nothing
 
